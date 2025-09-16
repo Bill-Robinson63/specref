550d1e7d73b9a5839332fcf1a355675f94805196
william.f.robinson.jr@gmail.com <william.f.robinson.jr@gmail.com>
2025-09-16 23:34:41 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,5 +7,10 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.waveColor = [[]]
+
+  def canvas_1_reset(self, **event_args):
+    """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
+    pass
 
     

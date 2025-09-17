6251705689993b615632fb8fdc11633f0ece968d
william.f.robinson.jr@gmail.com <william.f.robinson.jr@gmail.com>
2025-09-17 19:06:57 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,13 +7,22 @@ class Form1(Form1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.canvas_1.reset_context()
-    
-    self.waveColor = [[]]
+
+    self.spectralLines = {}
+    self.spectralLines["H_alpha"] = [[656, ], [486, ], [434, ], [410, ]]
+
+  def displayLine()
 
   def canvas_1_reset(self, **event_args):
     """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
     if self.H_alpha:
-      
+      self.canvas_1.begin_path()
+      self.canvas_1.move_to(100, 100)
+    self.canvas_1.line_to(150, 100)
+    self.canvas_1.line_to(150, 150)
+    self.canvas_1.line_to(100, 150)
+    self.canvas_1.close_path()
+    self.canvas_1.stroke()"""
 
   def H_alpha_change(self, **event_args):
     """This method is called when this checkbox is checked or unchecked"""

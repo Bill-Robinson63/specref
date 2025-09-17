cd29361cc7eeb6e9fe03349562195002b9129187
william.f.robinson.jr@gmail.com <william.f.robinson.jr@gmail.com>
2025-09-17 18:49:25 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,21 +2,21 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import stripe.checkout
 
-
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    self.canvas_size = 500
-    self.canvas_1.height = self.canvas_size
     self.canvas_1.reset_context()
-    self.star = [100, 100, 1, 2]
     
     self.waveColor = [[]]
 
   def canvas_1_reset(self, **event_args):
     """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
-    pass
+    if self.H_alpha:
+      
+
+  def H_alpha_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    self.canvas_1_reset()
 
     

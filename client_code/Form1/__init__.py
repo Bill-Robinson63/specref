75b7ecdc70db5a0c232e9765c74c2ee1492d3fac
william.f.robinson.jr@gmail.com <william.f.robinson.jr@gmail.com>
2025-09-17 17:03:57 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,6 +7,12 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    self.canvas_size = 500
+    self.canvas_1.height = self.canvas_size
+    self.canvas_1.reset_context()
+    self.star = [100, 100, 1, 2]
+    
     self.waveColor = [[]]
 
   def canvas_1_reset(self, **event_args):

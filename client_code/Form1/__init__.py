b028ed368e8c9d32e2eda0c2e8f5162e78024e8d
william.f.robinson.jr@gmail.com <william.f.robinson.jr@gmail.com>
2025-09-20 21:35:55 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,24 +12,27 @@ class Form1(Form1Template):
 
     # Let's start off with Hα, Hβ, O_III, O_I, S_II, N_II which are said to be most common/prominent for ameture astronomers
     self.spectralLines = {}
-    self.spectralLines["Hα"] = [[656, "#FF0000"], [486, "#00EFFF"], [434, "#2800FF"], [410, "#7E00DB"]]
+    self.spectralLines["H_alpha"] = [656, "#FF0000"]
+    self.spectralLines["H_beta"]  = [486, "#00EFFF"]
+    self.spectralLines["H_gamma"] = [434, "#2800FF"]
+    self.spectralLines["H_delta"] = [410, "#7E00DB"]
 
   def displayLine(self, lineKey):
     canvas = self.canvas_1
-    lineLength = canvas.get_width()
-    lineData = self.spectralLines[lineKey]
-    for line in lineData:
-      canvas.begin_path()
-      canvas.move_to(0, line[0] - 380)
-      canvas.line_to(lineLength, line[0] - 380)
-      canvas.stroke_style = line[1]
-      canvas.line_width = 1
-      canvas.stroke()
-      canvas.close_path()
+    xRatio = canvas.get_width() / 400
+    lineLength = canvas.get_height()
+    line = self.spectralLines[lineKey]
+    canvas.begin_path()
+    canvas.move_to(int((line[0] - 380) * xRatio), 0)
+    canvas.line_to(int((line[0] - 380) * xRatio), lineLength)
+    canvas.stroke_style = line[1]
+    canvas.line_width = int(xRatio)
+    canvas.stroke()
+    canvas.close_path()
 
   def canvas_1_reset(self, **event_args):
     """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
-    canWidth = self.canvas_1.get_width()
+    canWidth  = self.canvas_1.get_width()
     canLength = self.canvas_1.get_height()
     self.canvas_1.begin_path()
     self.canvas_1.move_to(0, 0)
@@ -41,8 +44,22 @@ class Form1(Form1Template):
     self.canvas_1.fill()
     
     if self.H_alpha.checked:
-      Form1.displayLine(self, "Hα")
+      Form1.displayLine(self, "H_alpha")
+    if self.H_beta.checked:
+      Form1.displayLine(self, "H_beta")
+    if self.H_gamma.checked:
+      Form1.displayLine(self, "H_gamma")
+    if self.H_delta.checked:
+      Form1.displayLine(self, "H_delta")
 
   def H_alpha_change(self, **event_args):
-    """This method is called when this checkbox is checked or unchecked"""
+    self.canvas_1_reset()
+
+  def H_beta_change(self, **event_args):
+    self.canvas_1_reset()
+
+  def H_gamma_change(self, **event_args):
+    self.canvas_1_reset()
+
+  def H_delta_change(self, **event_args):
     self.canvas_1_reset()

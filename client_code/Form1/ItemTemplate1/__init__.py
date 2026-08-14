94e05226742e6fb9ad9d046befc513b3a2d216a6
wfr63@icloud.com <wfr63@icloud.com>
2026-08-14 21:01:51 +0000

    Created Form1.ItemTemplate1; Edited Form1

---
+++ client_code/Form1/ItemTemplate1/__init__.py
@@ -0,0 +1,12 @@
+from ._anvil_designer import ItemTemplate1Template
+from anvil import *
+import anvil.server
+import stripe.checkout
+
+
+class ItemTemplate1(ItemTemplate1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    super().__init__(**properties)
+
+    # Any code you write here will run before the form opens.

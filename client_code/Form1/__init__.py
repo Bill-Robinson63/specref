8cceb87f2078a682abdfde093a189f562accbd38
wfr63@icloud.com <wfr63@icloud.com>
2026-08-16 23:39:13 +0000

    Edited settings and Form1; Deleted Pong

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,67 +1,45 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.server
 import stripe.checkout
-from anvil_extras import animation 
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.canvas_size = 600
-    self.canvas_1.height = self.canvas_size
-    self.star = [300, 300, 1, 2]
-    self.canvas_1.reset_context()
+    self.Mets_blue   = "#002D72"
+    self.Mets_orange = "#FF5910"
 
+  def Penrose_Grid(self, centerX, centerY, size):
+    c = self.canvas_1
+    c.line_width = 3
+    c.stroke_style = self.Mets_orange
+    c.fill_style = "black"
+    c.begin_path()
+    c.move_to(centerX, 0)
+    c.line_to(centerX + size, size)
+    c.line_to(centerX, 2*size)
+    c.line_to(centerX - size, size)
+    c.close_path()
+    c.fill()
+    c.stroke()
+
+    # time lines
+    #for t in range(0, 2*size, size/3):
+      #for s in range(centerX - size, centerX + size):
+        # u = 
+        # c.fill_rect(s, t, 1, 1, "magenta")
+
+  @handle("canvas_1", "reset")
   def canvas_1_reset(self, **event_args):
-    """This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form."""
-    # Adjust these coordinates if you want the drawing area to not be centered
-    self.canvas_offset = (self.canvas_1.get_width() - self.canvas_size)/2
-    self.canvas_1.translate(self.canvas_offset, 0)
-
-    # Restrict drawing to the section that we want visible
-    self.canvas_1.begin_path()
-    self.canvas_1.move_to(0, 0)
-    self.canvas_1.line_to(self.canvas_size, 0)
-    self.canvas_1.line_to(self.canvas_size, self.canvas_size)
-    self.canvas_1.line_to(0, self.canvas_size)
-    self.canvas_1.close_path()
-    self.canvas_1.fill_style = "Black"
-    self.canvas_1.fill()
-    self.canvas_1.clip()
-
-    """# Draw a square
-    self.canvas_1.begin_path()
-    self.canvas_1.move_to(100, 100)
-    self.canvas_1.line_to(150, 100)
-    self.canvas_1.line_to(150, 150)
-    self.canvas_1.line_to(100, 150)
-    self.canvas_1.close_path()
-    self.canvas_1.stroke()"""
-
-    # Draw a star
-    self.canvas_1.begin_path()
-    self.canvas_1.arc(self.star[0], self.star[1], 2)
-    self.canvas_1.close_path()
-    self.canvas_1.fill_style = "White"
-    self.canvas_1.fill()
-
-  def timer_1_tick(self, **event_args):
-    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-    self.star[0] += self.star[2]
-    self.star[1] += self.star[3]
-    if self.star[0] <= 0 or self.star[0] >= self.canvas_size:
-      self.star[2] = -self.star[2]
-    if self.star[1] <= 0 or self.star[1] >= self.canvas_size:
-      self.star[3] = -self.star[3]
-
-    self.canvas_1.begin_path()
-    self.canvas_1.arc(self.star[0], self.star[1], 2)
-    self.canvas_1.close_path()
-    self.canvas_1.fill_style = "White"
-    self.canvas_1.fill()
-    self.canvas_1.reset_context()
-  
+    #This method is called when the canvas is reset and cleared, such as when the window resizes, or the canvas is added to a form.
+    c = self.canvas_1
+    if c.get_height() > c.get_width():
+      canSize = c.get_width()
+    else:
+      canSize = c.get_height()
+    canMid = c.get_width() / 2
+    self.Penrose_Grid(canMid, int(canSize/2), int(canSize/2))
+    
   @handle("link_1", "click")
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
@@ -70,4 +48,8 @@ class Form1(Form1Template):
   @handle("button_1", "click")
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form("Pong")
+    pass
+    #open_form("")
+
+  
+
